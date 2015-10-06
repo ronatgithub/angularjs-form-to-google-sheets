a66e1d8089034306c11b79a8e2386d66083cbb0a
ronatgithub <ronny.mombasa@gmail.com>
2015-10-07 00:46:44 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 /**
  * AngularJS module to process a form.
  */
-angular.module('myApp', ['ajoslin.promise-tracker'])
+angular.module('myApp', ['ajoslin.promise-tracker','720kb.datepicker'])
   .controller('help', function ($scope, $http, $log, promiseTracker, $timeout) {
     $scope.subjectListOptions = {
-      'bug': 'Report a Bug',
-      'account': 'Account Problems',
-      'mobile': 'Mobile',
-      'user': 'Report a Malicious User',
-      'other': 'Other'
+      'Chef_inclusive': 'I want a Private Chef inclusive',
+      'Bed+Breakfast': 'I want only Breakfast to be inclusive',
+      'special': 'I am interested in customized and bespoke arrangements'
     };
+    // handle the show/hide of message input
+    $scope.subjectList = 'room_only';
 
     // Inititate the promise tracker to track form submissions.
     $scope.progress = promiseTracker();
@@ -24,26 +24,46 @@ angular.module('myApp', ['ajoslin.promise-tracker'])
         return;
       }
 
+      // function to make a ISO timestamp string
+      function displayTime() {
+          var str = "";
+          var date = new Date();
+          var str = date.toISOString();
+          return str;
+      };
+
+      // function to check if comments has a value, if not set a value
+      function comments() {
+        if ($scope.comments === undefined) {
+          var str ='null';
+          return str;
+        } else {
+          return $scope.comments;
+        }
+      };
+
       // Default values for the request.
-      var config = {
-        params : {
-          'callback' : 'JSON_CALLBACK',
+      var formData = {
+          'timestamp' : displayTime(),
           'name' : $scope.name,
           'email' : $scope.email,
           'subjectList' : $scope.subjectList,
-          'url' : $scope.url,
-          'comments' : $scope.comments
-        },
+          'pax' : $scope.pax,
+          'in' : $scope.in,
+          'out' : $scope.out,
+          'comments' : comments()
       };
 
-      // Perform JSONP request.
-      var $promise = $http.jsonp('response.json', config)
-        .success(function(data, status, headers, config) {
-          if (data.status == 'OK') {
+      // Perform request.      
+      var $promise = $http.post('https://sheetsu.com/apis/11e7a739', formData)
+        .success(function(data, status, headers) {
+          if (data.status == '201') {
             $scope.name = null;
             $scope.email = null;
             $scope.subjectList = null;
-            $scope.url = null;
+            $scope.pax = null;
+            $scope.in = null;
+            $scope.out = null;
             $scope.comments = null;
             $scope.messages = 'Your form has been sent!';
             $scope.submitted = false;
@@ -52,7 +72,7 @@ angular.module('myApp', ['ajoslin.promise-tracker'])
             $log.error(data);
           }
         })
-        .error(function(data, status, headers, config) {
+        .error(function(data, status, headers, formData) {
           $scope.progress = data;
           $scope.messages = 'There was a network error. Try again later.';
           $log.error(data);
@@ -67,4 +87,23 @@ angular.module('myApp', ['ajoslin.promise-tracker'])
       // Track the request and show its progress to the user.
       $scope.progress.addPromise($promise);
     };
-  });
+  })
+  // Remember that for a correct form post, the Content-Type header must be changed.
+  // To do this globally for all POST requests, this code can be used:
+  // from http://stackoverflow.com/questions/11442632/how-can-i-post-data-as-form-data-instead-of-a-request-payload
+  .config(['$httpProvider', function ($httpProvider) {
+    // Intercept POST requests, convert to standard form encoding
+    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $httpProvider.defaults.transformRequest.unshift(function (data, headersGetter) {
+      var key, result = [];
+
+      if (typeof data === "string")
+        return data;
+
+      for (key in data) {
+        if (data.hasOwnProperty(key))
+          result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+      }
+      return result.join("&");
+    });
+  }]);
